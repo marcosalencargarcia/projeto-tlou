dce04ec571812201e835de5bcc1ed7f5060b7d67
Marcos Antonio Alencar Garcia <118910310+marcosalencargarcia@users.noreply.github.com>
2022-12-19 20:26:08 +0000

    Menu gaveta inserido

--- script/script.js
+++ script/script.js
@@ -1,19 +1,19 @@
-let radio = document.getElementsByName('image')
+let radio = document.getElementsByName('rimage')
 let backg = document.querySelector('.backg')
+let divRadio = document.querySelector('.radios')
 
+divRadio.addEventListener("click", radios)
 
 function radios() {
     
     if (radio[0].checked) {
-        backg.style.backgroundImage = "../imagens/second-home-background.jpg"
+        backg.style.backgroundImage = "url(../imagens/first-home-background.jpg)"
     } else if (radio[1].checked) {
         backg.style.background = "url(../imagens/second-home-background.jpg)"
     } else if (radio[2].checked) {
-        window.alert('Olá')
-        window.prompt('Olá prompt')
+        backg.style.background = "url(../imagens/third-home-background.jpg)"
     } else if (radio[3].checked) {
-        window.alert('Olá')
-        window.prompt('Olá prompt')
+        backg.style.background = "url(../imagens/fourth-home-background.jpg)"
     }
 
 }
@@ -21,11 +21,17 @@ function radios() {
 
 function menuPrincipal() {
 
-    if (menu.style.display == 'block') {
-        menu.style.display = 'none'
+    if (menu.style.top == '150px') {
+        menu.style.top = '-100%'
     } else {
-        menu.style.display = 'block'
+        menu.style.top = '150px'
     }
+
+    /*
+    top: 150px;
+    left: 50%;
+    transform: translate(-50%, 0%);
+    */
 }
 
 
