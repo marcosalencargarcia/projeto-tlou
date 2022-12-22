c10ba50a34c66704610107011988c2753f370e7f
Marcos Antonio Alencar Garcia <118910310+marcosalencargarcia@users.noreply.github.com>
2022-12-22 00:31:21 +0000

    fixed others bugs videos

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ let divRadio = document.querySelector('.radios')
 const btnMenu = document.getElementById('btn-menu-hamb')
 let imgv = document.getElementsByClassName('img-video')
 let divVideos = document.querySelector('section#third > div.backg > div:nth-child(1)')
-var px = 50
+var px = 43 //margem esquerda da primeira imagem do carrossel em pixels
 
 divRadio.addEventListener("click", radios)
 imgv[0].addEventListener("click", clickimagem1)
@@ -106,20 +106,24 @@ function clickMudaImagemEsquerda() {
 }
 
 function clickMudaImagemVideo() {
-
+//cada imagem irá correr 274px para ser posicionado na div carrossel
     if (px < 0) {
-        px += 275
+        px += 274
         imgv[0].style.marginLeft = `${px}px`
+    } else {
+        imgv[0].style.marginLeft = `-779px`
+        px = -779
     }
 }
 
 function clickMudaImagemVideoDireita() {
-
-    if (px > -795 + 50) {
-        px += -275
+//cada imagem irá correr 274px para ser posicionado na div carrossel
+    if (px > -779 + 43) {
+        px += -274
         imgv[0].style.marginLeft = `${px}px`
     } else {
-        imgv[0].style.marginLeft = `50px`
+        imgv[0].style.marginLeft = `43px` //reset do posicionamento inicial
+        px = 43
     }
 }
 
