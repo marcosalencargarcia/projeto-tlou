d5ea2370104319a3ab9bc453c0583d3b6bed5bb5
Marcos Antonio Alencar Garcia <118910310+marcosalencargarcia@users.noreply.github.com>
2023-01-11 13:23:05 +0000

    Fixed bugs img

--- script/script.js
+++ script/script.js
@@ -156,6 +156,17 @@ function changeInfectedLeft() {
     }
 }
 
+function parallax(){
+    const paral = document.querySelector(".backg");
+        if(paral != null){
+        window.addEventListener("scroll", ()=> {
+          let offset = window.pageYOffset - paral.offsetTop + 10;
+          paral.style.backgroundPositionY = offset * 0.1 + "px";
+        });
+      }
+    }
+    parallax();
+
 // function clickMudaImagemVideo() {
 // //cada imagem irá correr 274px para ser posicionado na div carrossel
 //     if (px < 0) {
