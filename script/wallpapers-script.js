ba6f1f94ea2684bafa968653c0823f35555241ef
Marcos Antonio Alencar Garcia <118910310+marcosalencargarcia@users.noreply.github.com>
2023-01-14 16:14:39 +0000

    add new feature to open images

--- script/wallpapers-script.js
+++ script/wallpapers-script.js
@@ -1,7 +1,23 @@
-let wallDesktop = document.querySelectorAll('.desktop img')
+let wallDesktop = document.querySelectorAll('.desktop img, .phone img')
+let popup = document.querySelector('.popup-wrapper')
+let imgPopup = document.querySelector('.popup-content img')
+let popupclose = document.querySelector('.popup-close')
 
 wallDesktop.forEach(element => {
     element.addEventListener("click", (event) => {
-        console.log('testes', event);
+        popup.style.display = 'block'
+        imgPopup.src = event.currentTarget.src
     })
 });
+
+popupclose.addEventListener("click", () => {
+    popup.style.display = 'none'
+})
+
+popup.addEventListener("click", (event) => {
+    console.log('', event)
+    if (event.target === event.currentTarget) {
+        popup.style.display = 'none'
+    }
+})
+
